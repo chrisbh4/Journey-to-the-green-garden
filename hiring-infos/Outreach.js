e41eca950281ead7441a97475d90f10c3dfccb39
Chrisbh4 <chrismbh4@gmail.com>
2022-03-09 10:52:20 -0800

    new info from allie

---
+++ hiring-infos/Outreach.js
@@ -0,0 +1,88 @@
+/*
+
+React Linkedin job
+linkedin.com/jobs/search/?f_C=2751%2C14522%2C1476177%2C14775209%2C150884%2C1965675%2C2112%2C24173%2C2537812%2C2752%2C3139337%2C3201718%2C34274%2C3476896%2C3536161%2C404429%2C5169%2C7431%2C86849&f_E=2&f_T=9%2C8632&geoId=92000000&keywords=react&location=Worldwide&sortBy=R
+
+* Avoid recruiting managers
+
+! if message option on linkedin means i can send a free message
+
+* Message for Recruiter
+Hi <name> and Happy <current Day>,
+Expedia is a huge name in both technology and travel, and as a use of Expedia =, this is one of the comapines I owuld love to work for.
+I'm a software engineeer specializing in web development using tech such as React, Javascript, and Node, all of which align with the s
+kills posted in this job post for Software Development Engineer : <linik>
+
+If you have the time, I would appreciate you checking out my portfolio, and if you don't( or are not the right person to be reaching out to ) I would be grateful if you could redirect me or my message to the appropritate
+
+
+*Allies  Full Messages to Recruiters :
+ Expedia is a huge name in both technology and travel, and as a use of Expedia, this is one of the companies I would love to work for. I'm a software engineer specializing web development using tech such as React, Javascript, and Node, all of which align with the skills posted in this job post for Software Development Engineer II: https://www.linkedin.com/jobs/view/2904408122
+
+If you have the time, I would appreciate you checking out my portfolio, and if you don't (or are not the right person to be reaching out to) I would be so grateful if you could redirect me or my message to the appropriate person.
+
+Thank you for your time and all you do,
+
+Allie Villarreal
+
+
+* Recruiter Connection Message limited to 300 chars
+Don't know you personally YET but early career SWE here, shares the values of Expedia, and fits the SDE II role pretty strongly. Would love to connect, but if not thanks for your time. Best, Allie
+
+
+
+* Emails to recruiters
+! Subject : Following up // because we will already have messaged them on linkedin
+Hello <name>,
+
+I reachedo ut recently on Linkedin, but figured i should follow up via email if you missied it.
+I recetnly applied to this postion <link> and wanted to highlight my candidacy if possible as I am very comfortable with the technologies mentioned (specifically react and node.js )
+
+would love to hop on a call and talk more about my qualifications and how I might fit into the mission of <comapny name>
+
+if this message is better directed elsewhere, I ghreatly appreciate your time and any redirection you might provide.
+
+Otherwide , have a great day and hope to hear from you soon.
+
+* Allies Email : copy n paste
+
+Hello Russell, 
+
+I reached out recently on LinkedIn, but figured I should follow up via email if you missed it. 
+I recently applied to this position: [link] and wanted to highlight my candidacy if possible as I am very comfortable with the technologies mentioned (specifically react and node.js). 
+
+Would love to hop on a call and talk more about my qualifications and how I might fit into the mission of Expedia Group. 
+
+If this message is better directed elsewhere, I greatly appreciate your time and any redirection you might provide. 
+
+Otherwise, have a great day and hope to hear from you soon.
+
+--
+Allie VillarrealBehavioral Coach - OnlinePlacements Team
+
+
+* Allies Email pt 2 :
+
+Hello Rachel!
+
+
+Hope you're doing well. I saw this posting from Wellthy: [link] and after reading through the JD, I feel I would be a great fit on your team. I have experience with React, NoSQL, and Django, but as part of my bootcamp program, we also delved into Javascript, Python, and more.
+
+
+
+I have a passion for learning new technologies and, thanks to my experience, have both learned and built with various tools in lightning-fast turnaround times. I know whatever technology Wellthy is exploring, I would love to be on that journey with you all.
+
+
+If you have the time, here's my portfolio [link] which has a link to my resume as well. Would love to hop on a call and talk about the work at Wellthy and how I might contribute.
+
+
+Thanks and chat soon!
+
+Allie
+
+
+
+
+
+
+*/
