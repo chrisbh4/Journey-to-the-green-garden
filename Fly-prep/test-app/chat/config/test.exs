c3c9f32dd03297d3b5e70cf1b288e1ddba7acd7b
Chrisbh4 <chrismbh4@gmail.com>
2022-04-16 18:26:49 -0700

    learning elixir

---
+++ Fly-prep/test-app/chat/config/test.exs
@@ -0,0 +1,18 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chat, ChatWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "mj59ZDTbs2Wgs4nytdbdaSYv4Fby4WSOk6zbNq0ZK0GZb6t/Pu8I88y1K9C9MbIy",
+  server: false
+
+# In test we don't send emails.
+config :chat, Chat.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ Fly-prep/test-app/chat/lib/chat.ex
@@ -0,0 +1,15 @@
+defmodule Chat do
+  @moduledoc """
+  Chat keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+
+  """
+  # def handle_event("random-room", _params, socket) do
+  #   {:noreply, socket}
+  # end
+
+
+end
