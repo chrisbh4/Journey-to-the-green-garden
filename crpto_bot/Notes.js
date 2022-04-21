057e8587f501bf79821a030524d5c199c7b2d13c
Chrisbh4 <chrismbh4@gmail.com>
2022-04-21 10:49:26 -0700

    crypto bot notes

--- crpto_bot/Notes.js
+++ crpto_bot/Notes.js
@@ -12,6 +12,17 @@
             docker-compose run freqtrade hyperopt -s BBRSIStrategy --epochs 5000 --hyperopt-loss SharpeHyperOptLoss
 
 
+* Hyperopt Methods
+- OnlyProfitHyperOptLoss - takes only amount of profit into consideration.
+- ShortTradeDurHyperOptLoss - (default legacy Freqtrade hyperoptimization loss function) - Mostly for short trade duration and avoiding losses.
+- SharpeHyperOptLoss - optimizes Sharpe Ratio calculated on trade returns relative to standard deviation.
+- SharpeHyperOptLossDaily - optimizes Sharpe Ratio calculated on daily trade returns relative to standard deviation.
+- SortinoHyperOptLoss - optimizes Sortino Ratio calculated on trade returns relative to downside standard deviation.
+- SortinoHyperOptLossDaily - optimizes Sortino Ratio calculated on daily trade returns relative to downside standard deviation.
+- MaxDrawDownHyperOptLoss - Optimizes Maximum drawdown.
+- CalmarHyperOptLoss - Optimizes Calmar Ratio calculated on trade returns relative to max drawdown.
+- ProfitDrawDownHyperOptLoss - Optimizes by max Profit & min Drawdown objective. DRAWDOWN_MULT variable within the hyperoptloss file can be adjusted to be stricter or more flexible on drawdown purposes.
+
 
 
 
