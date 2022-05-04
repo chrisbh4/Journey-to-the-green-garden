80ee139797c016d7f674066b09c9db8963774e9d
Chrisbh4 <chrismbh4@gmail.com>
2022-05-04 09:58:26 -0700

    learning how to build a form in liveview

--- elixir-school/first_ex_proj/chat/lib/chat_web/controllers/room_live_view.ex
+++ elixir-school/first_ex_proj/chat/lib/chat_web/controllers/room_live_view.ex
@@ -5,39 +5,35 @@ defmodule ChatWeb.RoomLive do
 
   # Need to render the form and trying to render chat.html with the def render function below
 
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1>Welcome to the chat room id: <%= @room_id %> </h1>
+      <button phx-click="back_home">Home</button>
 
+      <div id="chat-container">
+        <div id="chat-messages">
+          Chat goes here
+         </div>
+      </div>
+
+    </div>
+    """
+  end
+  # Need to understand how to get a form to work on liveview
+
+
+
+  
   # def render(assigns) do
-  #   ~H"""
-  #  <h1>Welcome to the chat room id: <%= @room_id %> </h1>
-    # <button phx-click="back_home">Home</button>
-
-    # <div id="chat-container">
-    #   <div id="chat-messages">
-    #     Chat goes here
-    #   </div>
-
-    #   <.form let={f} for={:chat} id="chat-form" phx-submit={:submit_message}>
-    #     <%# <%= text_input f, :message, placeholder: "Enter your message...." %> %>
-    #   </.form>
-    # </div>
-  #   """
+  #   Phoenix.View.render( ChatWeb.ChatLive, "chat.html", assigns)
   # end
 
-  def render(assigns) do
-    Phoenix.View.render( ChatWeb.ChatLive, "chat.html", assigns)
-  end
-
-
-
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok , assign( socket , room_id: room_id )}
+    {:ok, assign(socket, room_id: room_id)}
   end
 
-  def handle_event("back_home",_params, socket)do
+  def handle_event("back_home", _params, socket) do
     {:noreply, push_redirect(socket, to: "/")}
   end
-
-
-
-
 end
