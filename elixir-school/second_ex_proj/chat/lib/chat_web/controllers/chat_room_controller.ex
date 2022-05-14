591352abbf400fa4c6468bcafb0f5477c04f1437
Chrisbh4 <chrismbh4@gmail.com>
2022-05-14 13:37:16 -0700

    new messages functioality

--- elixir-school/second_ex_proj/chat/lib/chat_web/controllers/chat_room_controller.ex
+++ elixir-school/second_ex_proj/chat/lib/chat_web/controllers/chat_room_controller.ex
@@ -6,17 +6,31 @@ defmodule ChatWeb.ChatRoomLiveController do
 
   def render(assigns) do
     ~H"""
+    <section class="phx-hero">
     <h1> Inside Chat Room </h1>
+    <button phx-click="home-button">Home</button>
 
-
+    <.form let={f} for={:chat} phx-submit={:new_message}>
+      <%= text_input f, :message_input %>
+    </.form>
+    </section>
     """
   end
+# Create form and have it be able to submit new messages that log into the server
+
 
   def mount(_params, socket) do
     Logger.info("Inside Chat room")
     {:noreply, socket}
   end
 
+  def handle_event("home-button", _params, socket) do
+    Logger.info("Going back home!")
+    {:noreply, push_redirect(socket, to: "/")}
+  end
 
-
+  def handle_event(event: "new-message", payload: message, socket) do
+    Logger.info(:payload , message_input)
+    {:noreply, socket}
+  end
 end
