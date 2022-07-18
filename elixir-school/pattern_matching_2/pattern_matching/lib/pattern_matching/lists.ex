055db3af5fba65604a5625088dfc9013c8c8ddc9
Christian Brown <chrismbh4@gmail.com>
2022-07-18 11:37:59 -0700

    List patterns solved

--- elixir-school/pattern_matching_2/pattern_matching/lib/pattern_matching/lists.ex
+++ elixir-school/pattern_matching_2/pattern_matching/lib/pattern_matching/lists.ex
@@ -11,32 +11,31 @@ defmodule PatternMatching.Lists do
   Fix or complete the code to make the tests pass.
   """
 
-  def is_empty?(_list) do
+  def is_empty?([]), do: true
+  def is_empty?(_other), do: false
 
+  def has_1_item?([_]), do: true
+  def has_1_item?(_other),do: false
+
+  def at_least_one?([_ | _rest]), do: true
+  def at_least_one?(_list), do: false
+
+
+
+  def return_first_item([first | _rest]), do: first
+  def return_first_item(_list), do: :error
+
+  def starts_with_1?([1 | _rest]), do: true
+  def starts_with_1?(_), do: false
+
+  def sum_pair([first, second]), do: first + second
+  def sum_pair(_list), do: :error
+
+  def sum_first_2([first, second |rest]) do
+    sum = first + second
+    [sum] ++ rest
   end
 
-  def has_1_item?(_list) do
-
-  end
-
-  def at_least_one?(_list) do
-
-  end
-
-  def return_first_item(_list) do
-
-  end
-
-  def starts_with_1?(_list) do
-
-  end
-
-  def sum_pair(_list) do
-
-  end
-
-  def sum_first_2(_list) do
-
-  end
+  def sum_first_2(list), do: list
 
 end
