93184cd41b7ada08aaee1dfab1ca64d3514b43dc
Christian Brown <chrismbh4@gmail.com>
2022-07-20 19:17:38 -0700

    guard classes almost finished

--- elixir-school/pattern_matching_1/lib/pattern_matching/guard_clauses.ex
+++ elixir-school/pattern_matching_1/lib/pattern_matching/guard_clauses.ex
@@ -12,29 +12,33 @@ defmodule PatternMatching.GuardClauses do
   """
   alias PatternMatching.User
 
-  def return_numbers(_value) do
-    
+  def return_numbers(value) when is_float(value) do
+    value
   end
-
-  def return_lists(_value) do
-
+  def return_numbers(value) when is_number(value) do
+    value
   end
+  def return_numbers(_), do: :error
 
-  def return_any_size_tuples(_value) do
+  def return_lists(list) when is_list(list), do: list
+  def return_lists(_list), do: :error
 
-  end
 
-  def return_maps(_value) do
+  def return_any_size_tuples(tuple) when is_tuple(tuple), do: tuple
+  def return_any_size_tuples(_), do: :error
 
-  end
 
-  def run_function(_value) do
+  def return_maps(maps) when is_map(maps), do: maps
+  def return_maps(_value), do: :error
 
-  end
+  def run_function(func) when is_function(func), do: func.()
+  def run_function(_value), do: :error
 
-  def classify_user(_user) do
-
-  end
+  def classify_user(%User{age: age} = _user) when age >= 18, do: {:ok, :adult}
+  def classify_user(%User{age: age} = _user) when age < 0 , do: {:error, "Age cannot be negative"}
+  def classify_user(%User{name: _name} = _user), do: {:error, "Age missing"}
+  def classify_user(%User{age: age} = _user) when age < 18, do: {:ok, :minor}
+  def classify_user(_other), do: {:error , "Not a user"}
 
   def award_child_points(_user, _max_age, _points) do
 
