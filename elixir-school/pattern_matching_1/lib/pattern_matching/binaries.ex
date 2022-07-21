df6c7a378ffc747453e59169a9aefcc54f58c4ed
Christian Brown <chrismbh4@gmail.com>
2022-07-20 18:48:58 -0700

    binaries patterns solved

--- elixir-school/pattern_matching_1/lib/pattern_matching/binaries.ex
+++ elixir-school/pattern_matching_1/lib/pattern_matching/binaries.ex
@@ -11,16 +11,34 @@ defmodule PatternMatching.Binaries do
   Fix or complete the code to make the tests pass.
   """
 
-  def identify_command(_value) do
-
+  def identify_command("SAY " <> text) do
+    {:say ,text }
+  end
+  def identify_command("WAVE " <> text) do
+    {:wave ,text }
+  end
+  def identify_command(_) do
+    {:error, "Unrecognized command"}
   end
 
-  def format_phone(_value) do
-
+  def format_phone(<< first::binary-size(3), last::binary-size(4)>>) do
+    "#{first}-#{last}"
+  end
+  def format_phone(<< area::binary-size(3), first::binary-size(3), last::binary-size(4)>>) do
+    "(#{area}) #{first}-#{last}"
+  end
+  def format_phone(error) do
+    error
   end
 
-  def image_type(_value) do
+  @png_signature <<137::size(8), 80::size(8), 78::size(8), 71::size(8), 13::size(8), 10::size(8),
+  26::size(8), 10::size(8)>>
 
-  end
+@jpg_signature <<255::size(8), 216::size(8)>>
+
+def image_type(<< @png_signature, _rest::binary >>), do: :png
+def image_type(<< @jpg_signature, _rest::binary >>), do: :jpg
+def image_type(_other), do: :unknown
 
 end
+
