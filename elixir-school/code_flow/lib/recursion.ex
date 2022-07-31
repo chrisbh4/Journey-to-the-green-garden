f5d3426601e4345382eb75b62ae8eea05306723f
Christian Brown <chrismbh4@gmail.com>
2022-07-31 10:27:09 -0700

    recurssion problem 1 solved

--- elixir-school/code_flow/lib/recursion.ex
+++ elixir-school/code_flow/lib/recursion.ex
@@ -10,15 +10,31 @@ defmodule CodeFlow.Recursion do
   @moduledoc """
   Fix or complete the code to make the tests pass.
   """
-  # alias CodeFlow.Fake.Customers
-  # alias CodeFlow.Schemas.OrderItem
+  alias CodeFlow.Fake.Customers
+  alias CodeFlow.Schemas.OrderItem
 
   @doc """
   Sum a list of OrderItems to compute the order total.
   """
-  def order_total(_order_items) do
+  # total is computed as an Item’s price * the quantity
+  # Summing all of those together for the total order price
+  # {Item{active: true, id: nil, name: "Item 1", price: 5.0, quantity: 1, weight: 0.0}, quantity: 2}
 
+  # - split the list from [head| rest] which seperates the first item in the list
+  # - Key into OrderItem and access .price & .item.price and multiple togeter
+  #   - take the total of the multplication and sum it with total
+  # - pass the rest of the list and total into the recursive function again
+  # - create a function when passed a empty list and total return total
+
+  def order_total(order_items) do
+      do_order_total(order_items, 0)
   end
+
+  defp do_order_total([%OrderItem{}= item | rest], total) do
+      do_order_total(rest, (item.quantity * item.item.price) + total)
+  end
+
+  defp do_order_total([],total), do: total
 
   @doc """
   Count the number of active customers. Note: Normally this would be done with a
