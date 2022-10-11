9913730b08f86805a4c2d3c1fddd7ed7992d1cc6
Christian Brown <chrismbh4@gmail.com>
2022-10-11 08:51:30 -0700

    udemy cards proj created

---
+++ elixir-school/udemy/cards/lib/cards.ex
@@ -0,0 +1,20 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
+# Module 1 exersise 7
+# https://www.udemy.com/course/the-complete-elixir-and-phoenix-bootcamp-and-tutorial/learn/lecture/5427956#notes
